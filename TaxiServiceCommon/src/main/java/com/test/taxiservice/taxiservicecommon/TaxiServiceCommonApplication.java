a8e06df89424113e9b8cf7d1ef5d262ad7ebfaaf
I545318 <ganga.ramesh@sap.com>
2023-04-10 11:04:45 +0530

    Add new module for Common Functionalities - Redis cache used for storing OTP

---
+++ TaxiServiceCommon/src/main/java/com/test/taxiservice/taxiservicecommon/TaxiServiceCommonApplication.java
@@ -0,0 +1,11 @@
+package com.test.taxiservice.taxiservicecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiServiceCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiServiceCommonApplication.class, args);
+    }
+}
