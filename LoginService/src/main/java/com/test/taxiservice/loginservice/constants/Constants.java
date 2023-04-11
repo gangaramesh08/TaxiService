9c24629dc02ce6e6b0094a485d5de151045994c4
I545318 <ganga.ramesh@sap.com>
2023-04-11 14:55:22 +0530

    Added APIGateway + Added API for updating profile

--- LoginService/src/main/java/com/test/taxiservice/loginservice/constants/Constants.java
+++ LoginService/src/main/java/com/test/taxiservice/loginservice/constants/Constants.java
@@ -9,4 +9,15 @@ public class Constants {
     public static final String FIRST_NAME = "firstName";
     public static final String LAST_NAME = "lastName";
     public static final String MOBILE_NUMBER = "mobileNumber";
+
+    public static final String LOGINSERVICE_API_URL = "/loginservice";
+    public static final String LOGIN_API_URL = "/login";
+    public static final String SIGNUP_API_URL = "/signup";
+    public static final String VALIDATE_OTP_API_URL = "/validateOTP";
+    public static final String REGENERATE_OTP_API_URL = "/regenerateOTP";
+    public static final String PROFILE_UPDATE_API_URL = "/profile/update";
+
+    public static final long EXPIRY_DURATION = 24 * 60 * 60 * 1000;
+
+    public static final String BEARER = "Bearer ";
 }
